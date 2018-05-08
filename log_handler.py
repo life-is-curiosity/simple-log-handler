75d4cce3edc73fc2cee2255f76d4a47103244178
Allen Lancelot <alan910810@gmail.com>
2018-05-09 01:03:48 +0800

    Fixed bug
    The backup files will be generated more than limitation sometimes

--- log_handler.py
+++ log_handler.py
@@ -20,29 +20,33 @@ def get_map(start_path):
 
 # Handle Log behavior 
 def operation(path, file_map, backup_map, size=1, limit=1):
-    early_date = None
-    rm_file = ''
-    now_date_time = datetime.datetime.now()
-
     # Get files those have been more than <size> MB
     for n in dict(filter(lambda item: item[1] > size, file_map.items())).items():
+        rm_file = ''
+        early_date = None
+        now_date_time = datetime.datetime.now()
         path_and_name = '{}/{}'.format(path, n[0])
         print("{} -> Start to backup : {} ".format(now_date_time.strftime("%Y-%m-%d %H:%M:%S"), path_and_name))
 
         try:
+            # Get all related backup files 
+            backup_dict = dict(filter(lambda item: bool(re.compile(r'{}.*backup'.format(n[0])).match(item[0])), backup_map.items()))
+
+            print('backup_dict :--> {}'.format(backup_dict))
+
             # Backup the original log file as ZIP file
-            print("{} -> Zipping .... :{} ".format(now_date_time.strftime("%Y-%m-%d %H:%M:%S"), path_and_name))
+            print("{} -> Zipping .... :{} ".format(
+                now_date_time.strftime("%Y-%m-%d %H:%M:%S"), path_and_name))
             with zipfile.ZipFile('{}.zip'.format('{}/{}.{}.backup'.format(path, n[0], now_date_time.strftime("%Y-%m-%d_%H-%M-%S"))), 'w', zipfile.ZIP_DEFLATED) as backupZip:
                 backupZip.write(path_and_name, arcname=path_and_name)
-                size = sum([backupZip.file_size for backupZip in backupZip.filelist])
-            
+                size = sum(
+                    [backupZip.file_size for backupZip in backupZip.filelist])
+
             # Truncate the original log file
             with open(path_and_name, 'r+') as f:
                 f.truncate()
-            print("{} -> {} has been truncated".format(now_date_time.strftime("%Y-%m-%d %H:%M:%S"), path_and_name))
-
-            # Get all related backup files 
-            backup_dict = dict(filter(lambda item: bool(re.compile(r'{}.*backup'.format(n[0])).match(item[0])), backup_map.items()))
+            print("{} -> {} has been truncated".format(
+                now_date_time.strftime("%Y-%m-%d %H:%M:%S"), path_and_name))
 
             # Check if there are more than <limit> backup files
             if len(backup_dict) >= limit:
@@ -60,6 +64,7 @@ def operation(path, file_map, backup_map, size=1, limit=1):
                     del backup_map[rm_file]
                 except OSError as e:
                     print('IO Error {}'.format(e))
+
         except PermissionError as pe:
             print('{} -> The process cannot access the file because it is being used by another process'.format(now_date_time.strftime("%Y-%m-%d %H:%M:%S")))
 
